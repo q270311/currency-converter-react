59a4977c2b5eb2ccf24a584f9bdc6d5b4e51d585
Michał Bałdyga <q270311@gmail.com>
2022-11-25 09:48:22 +0100

    Footer component added

---
+++ src/Footer/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+const Footer = ({ value }) => (
+    <footer className="footer">
+        <p className="footer__paragraph">{value}</p>
+    </footer>
+);
+
+export default Footer;
