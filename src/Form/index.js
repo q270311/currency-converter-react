2793c08a754eedff81d481e9eb5555774c5db3e7
Michał Bałdyga <q270311@gmail.com>
2022-11-25 14:13:54 +0100

    Form interaction added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,23 +1,60 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./style.css";
 
 const Form = () => {
-    /* const [newTaskContent, setNewTaskContent] = useState("");
+    const [inputCurrencyRate, setInputCurrencyRate] = useState("4.8606");
+    const [inputCurrencySymbol, setInputCurrencySymbol] = useState("EUR");
+    const [outputCurrencyRate, setOutputCurrencyRate] = useState("4.8606");
+    const [outputCurrencySymbol, setOutputCurrencySymbol] = useState("EUR");
+    const onSelectInputChange = ({ target }) => {
+        setInputCurrencyRate(target.value);
+        setInputCurrencySymbol(target.options[target.selectedIndex].text);
+        calculate("");
+    }
+    const onSelectOutputChange = ({ target }) => {
+        setOutputCurrencyRate(target.value);
+        setOutputCurrencySymbol(target.options[target.selectedIndex].text);
+        calculate("");
+    }
+    const [currencyValue, setCurrencyValue] = useState("");
+    const [calculation, calculate] = useState("");
+
     const onFormSubmit = (event) => {
         event.preventDefault();
-        addNewTask(newTaskContent.trim());
-        setNewTaskContent("");
-    } */
+        calculate((currencyValue * inputCurrencyRate / outputCurrencyRate).toFixed(2));
+    }
+    const swapCurrencies = () => {
+        const tmpRate = inputCurrencyRate;
+        setInputCurrencyRate(outputCurrencyRate);
+        setOutputCurrencyRate(tmpRate);
+        const tmpSymbol = inputCurrencySymbol;
+        setInputCurrencySymbol(outputCurrencySymbol);
+        setOutputCurrencySymbol(tmpSymbol);
+        calculate("");
+    }
+
+    const Result = () => (
+        currencyValue !== "" && (
+            <p className="form__paragraph--result">
+                {currencyValue} {inputCurrencySymbol} = <strong>{calculation} {outputCurrencySymbol}</strong>
+            </p>
+        )
+    );
+
     return (
         <main>
-            <form className="form">
+            <form className="form" onSubmit={onFormSubmit}>
                 <label className="form__label">Kwota*:<br />
-                    <input type="number" name="amountOfMoney" required min="0" step="0.01" placeholder="Wpisz kwotę"
-                        className="form__element" />
+                    <input
+                        type="number" required min="0" step="0.01" placeholder="Wpisz kwotę"
+                        className="form__element"
+                        value={currencyValue}
+                        onChange={(event) => setCurrencyValue(event.target.value)}
+                    />
                 </label>
                 <label className="form__label">Mam:<br />
-                    <select className="form__element">
-                        <option value="4.8606" selected>EUR</option>
+                    <select className="form__element" value={inputCurrencyRate} onChange={onSelectInputChange}>
+                        <option value="4.8606">EUR</option>
                         <option value="4.9588">USD</option>
                         <option value="5.0065">CHF</option>
                         <option value="5.5505">GBP</option>
@@ -25,20 +62,25 @@ const Form = () => {
                     </select>
                 </label>
 
-                <span className="material-symbols-outlined form__element form__element--currencySwitch">sync_alt</span>
+                <span
+                    className="material-symbols-outlined form__element form__element--currencySwitch"
+                    onClick={swapCurrencies}
+                >
+                    sync_alt
+                </span>
 
                 <label className="form__label">Chcę otrzymać:<br />
-                    <select className="form__element">
+                    <select className="form__element" value={outputCurrencyRate} onChange={onSelectOutputChange}>
                         <option value="4.8606">EUR</option>
                         <option value="4.9588">USD</option>
                         <option value="5.0065">CHF</option>
                         <option value="5.5505">GBP</option>
-                        <option value="1.0" selected>PLN</option>
+                        <option value="1.0">PLN</option>
                     </select>
                 </label>
-                <input type="button" value="Przelicz" className="form__element form__element--button" />
+                <input type="submit" value="Przelicz" className="form__element form__element--button" />
             </form>
-            <p className="form__paragraph--result"></p>
+            <Result />
         </main>
     );
 };
