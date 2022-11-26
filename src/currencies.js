52e070f2c22a4994b3692c6bbcbc90fa6edf2c3f
Michał Bałdyga <q270311@gmail.com>
2022-11-26 21:40:57 +0100

    currencies table added

---
+++ src/currencies.js
@@ -0,0 +1,27 @@
+export const currencies = [
+    {
+        short: "PLN",
+        name: "polski złoty",
+        rate: 1.0,
+    },
+    {
+        short: "EUR",
+        name: "euro",
+        rate: 4.8606,
+    },
+    {
+        short: "USD",
+        name: "dolar amerykański",
+        rate: 4.9588,
+    },
+    {
+        short: "GBP",
+        name: "funt brytyjski",
+        rate: 5.5505,
+    },
+    {
+        short: "CHF",
+        name: "frank szwajcarski",
+        rate: 5.0065,
+    },
+];
