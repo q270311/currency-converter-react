c5787251c39e827a978183ab66749db50a8abfd7
Michał Bałdyga <q270311@gmail.com>
2022-11-26 16:40:04 +0100

    formated code

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import Header from "./Header";
 import Form from "./Form";
 import Footer from "./Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <Container>
-        <Header title="Kalkulator walutowy" />
-        <Form />
-        <Footer value="EUR 4.8606; USD 4.9588; CHF 5.0065; GBP 5.5505" />
-      </Container>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Container>
+      <Header title="Kalkulator walutowy" />
+      <Form />
+      <Footer value="EUR 4.8606; USD 4.9588; CHF 5.0065; GBP 5.5505" />
+    </Container>
+  </div>
+);
 
 export default App;
